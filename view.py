84b465020b9c964aaa6db26047a44fdbf496f0d5
Ro0t <tommaso.patriti@gmail.com>
2021-09-12 01:10:47 +0200

    view

--- view.py
+++ view.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import controller
+from controller import connection
 
 
 class Application(tk.Frame):
@@ -13,8 +13,29 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.next = tk.Button(self)
         self.next["text"] = "next"
-        self.next["command"] = controller.connection("MediaPlayer1.Next")
-        self.next.pack(side="top")
+        self.next["command"] = self.next_song
+        self.next.grid(row = 0, column = 2, padx = 10, pady= 10)
+        
+        self.pause_play = tk.Button(self)
+        self.pause_play["text"] = "|| >"
+        self.pause_play["command"] = self.prev_song
+        self.pause_play.grid(row = 0, column = 1, padx = 10, pady= 10)
+        
+        self.prev = tk.Button(self)
+        self.prev["text"] = "prev"
+        self.prev["command"] = self.prev_song
+        self.prev.grid(row = 0, column = 0, padx = 10, pady= 10)
+
+        
+        
+    def next_song(self):
+        connection("MediaControl1.Next")
+        
+    def prev_song(self):
+        connection("MediaControl1.Previous")
+        
+    def play_pause(self):
+        connection("MediaControl1.Previous")
 
 
 
